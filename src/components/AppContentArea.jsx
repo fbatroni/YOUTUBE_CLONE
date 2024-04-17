bec3d620cad8418a5393fe8a2d1a43968ac23e1d
Fritz G. Batroni <fritz.batroni@celeritycommerce.com>
2024-04-17 19:05:24 -0400

    end of part1

--- src/components/AppContentArea.jsx
+++ src/components/AppContentArea.jsx
@@ -1,10 +1,27 @@
 import Box from "@mui/material/Box";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SideList from "./SideList";
 import { appContentWrapper, flexColumnGrow } from "@styles/styles";
 import TabList from "./TabList";
+import CardList from "./CardList";
+import { fetchFromApi } from "../utils/api";
 
 const AppContentArea = ({ isOpen }) => {
+  const [youtubeData, setYoutubeData] = useState([]);
+
+  // categories
+  const [searchText, setSearchText] = useState("All");
+
+  useEffect(() => {
+    fetchFromApi("/search?part=snippet", searchText).then((response) => {
+      setYoutubeData(response.data.items);
+    });
+  }, [searchText]);
+
+  if (!youtubeData.length) {
+    return;
+  }
+
   const sideBarWidth = isOpen ? "70px" : "250px";
   return (
     <Box component="main" sx={appContentWrapper}>
@@ -21,12 +38,19 @@ const AppContentArea = ({ isOpen }) => {
       </Box>
       <Box component="div" sx={flexColumnGrow}>
         <Box
+          component="div"
           sx={{
-            my: 2,
+            display: "block",
+            p: 2,
+            minHeight: "100px",
+            mb: 4,
+            overflow: "hidden",
+            overflowY: "auto",
             width: `calc(100vw - ${sideBarWidth})`,
           }}
         >
           <TabList />
+          <CardList items={youtubeData} />
         </Box>
       </Box>
     </Box>
